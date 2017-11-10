f2ebfe4154315d7fcc6c226db99fd47969ca6023
Josh Poimboeuf <jpoimboe@redhat.com>
2017-11-10 12:44:24 -0600

    powerpc/module: Improve restore_r2() error message
    Print the function address associated with the restore_r2() error to
    make it easier to debug the problem.
    
    Also clarify the wording a bit.
    
    Before:
    
      module_64: patch_foo: Expect noop after relocate, got 3c820000
    
    After:
    
      module_64: patch_foo: Expected noop after call, got 7c630034 at netdev_has_upper_dev+0x54/0xb0 [patch_foo]
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>

--- arch/powerpc/kernel/module_64.c
+++ arch/powerpc/kernel/module_64.c
@@ -501,8 +501,8 @@ static int restore_r2(u32 *instruction, struct module *me)
 		return 1;
 
 	if (*instruction != PPC_INST_NOP) {
-		pr_err("%s: Expect noop after relocate, got %08x\n",
-		       me->name, *instruction);
+		pr_err("%s: Expected noop after call, got %08x at %pS\n",
+		       me->name, *instruction, instruction);
 		return 0;
 	}
 	/* ld r2,R2_STACK_OFFSET(r1) */
@@ -624,7 +624,8 @@ int apply_relocate_add(Elf64_Shdr *sechdrs,
 
 		case R_PPC_REL24:
 			/* FIXME: Handle weak symbols here --RR */
-			if (sym->st_shndx == SHN_UNDEF) {
+			if (sym->st_shndx == SHN_UNDEF ||
+			    sym->st_shndx == SHN_LIVEPATCH) {
 				/* External: go via stub */
 				value = stub_for_addr(sechdrs, value, me);
 				if (!value)
