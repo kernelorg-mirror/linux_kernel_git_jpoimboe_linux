9292ae2e25ab61de58c35bef33e7b1247910edaa
Sean Christopherson <sean.j.christopherson@intel.com>
2019-01-18 13:20:31 -0800

    KVM: VMX: Create a stack frame in vCPU-run
    ...in preparation for moving to a proper assembly sub-routnine.
    vCPU-run isn't a leaf function since it calls vmx_update_host_rsp()
    and vmx_vmenter().  And since we need to save/restore RBP anyways,
    unconditionally creating the frame costs a single MOV, i.e. don't
    bother keying off CONFIG_FRAME_POINTER or using FRAME_BEGIN, etc...
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -6406,8 +6406,8 @@ static void __vmx_vcpu_run(struct kvm_vcpu *vcpu, struct vcpu_vmx *vmx)
 		write_cr2(vcpu->arch.cr2);
 
 	asm(
-		/* Store host registers */
 		"push %%" _ASM_BP " \n\t"
+		"mov %%" _ASM_SP ", %%" _ASM_BP " \n\t"
 		"push %%" _ASM_ARG2 " \n\t"
 
 		/* Adjust RSP to account for the CALL to vmx_vmenter(). */
