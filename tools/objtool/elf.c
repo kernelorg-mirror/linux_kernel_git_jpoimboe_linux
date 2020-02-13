fd216f8df13fba519291595f2d993a9913beaa56
Josh Poimboeuf <jpoimboe@redhat.com>
2020-02-13 09:41:19 -0600

    objtool: Workaround binutils local label symbols bug
    TODO
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>

--- tools/objtool/elf.c
+++ tools/objtool/elf.c
@@ -63,7 +63,8 @@ struct symbol *find_symbol_by_offset(struct section *sec, unsigned long offset)
 
 	list_for_each_entry(sym, &sec->symbol_list, list)
 		if (sym->type != STT_SECTION &&
-		    sym->offset == offset)
+		    sym->offset == offset &&
+		    strncmp(sym->name, ".L", 2)) // binutils hack
 			return sym;
 
 	return NULL;
