ac9b9a0874609c0667cb8027182b29bc39958045
Josh Poimboeuf <jpoimboe@redhat.com>
2020-03-04 19:42:57 -0600

    x86/dumpstack: Add SHOW_REGS_IRET mode
    Now that __show_regs() has the concept of "modes" to indicate which
    registers should be displayed, replace show_iret_regs() with a new
    SHOW_REGS_IRET mode.  This is only a cleanup and doesn't change any
    behavior.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Reviewed-by: Miroslav Benes <mbenes@suse.cz>

--- arch/x86/include/asm/kdebug.h
+++ arch/x86/include/asm/kdebug.h
@@ -23,6 +23,7 @@ enum die_val {
 };
 
 enum show_regs_mode {
+	SHOW_REGS_IRET,
 	SHOW_REGS_SHORT,
 	/*
 	 * For when userspace crashed, but we don't think it's our fault, and
@@ -37,7 +38,6 @@ void die_addr(const char *str, struct pt_regs *regs, long err, long gp_addr);
 extern int __must_check __die(const char *, struct pt_regs *, long);
 extern void show_stack_regs(struct pt_regs *regs);
 extern void __show_regs(struct pt_regs *regs, enum show_regs_mode);
-extern void show_iret_regs(struct pt_regs *regs);
 extern unsigned long oops_begin(void);
 extern void oops_end(unsigned long, struct pt_regs *, int signr);
 
