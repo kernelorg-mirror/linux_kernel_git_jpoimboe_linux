b9dfe77dc170e354a54707866c23f81072f07043
Josh Poimboeuf <jpoimboe@redhat.com>
2021-11-05 09:54:52 -0700

    objtool: Prevent reachable/unreachable macro movement
    This patch causes these macros to break with Clang and
    CONFIG_TRACE_BRANCH_PROFILING.
    
    I get a lot of warnings like
    
      arch/x86/kernel/traps.o: warning: objtool: handle_xfd_event()+0x134: unreachable instruction
    
    Without an asm input, 'volatile' is ignored for some reason and Clang
    feels free to move the reachable() annotation away from its intended
    location.
    
    I wonder if we could go back to the original approach of providing
    __COUNTER__ as an input to the asm, but then mask it to < 128.
    
    XXX: Does this work on s390?
    XXX: Same for instr_begin/end macros
    XXX: Use asm_inline
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -115,24 +115,18 @@ void ftrace_likely_update(struct ftrace_likely_data *f, int val,
  * The __COUNTER__ based labels are a hack to make each instance of the macros
  * unique, to convince GCC not to merge duplicate inline asm statements.
  */
-#define __stringify_label(n) #n
-
-#define __annotate_reachable(c) ({					\
-	asm volatile(__stringify_label(c) ":\n\t"			\
+#define annotate_reachable() ({						\
+	asm volatile("%c0:\n\t"						\
 		     ".pushsection .discard.reachable\n\t"		\
-		     ".long " __stringify_label(c) "b - .\n\t"		\
-		     ".popsection\n\t");				\
+		     ".long %c0b - .\n\t"				\
+		     ".popsection\n\t" : : "i" (__COUNTER__ & 0x7f));	\
 })
-#define annotate_reachable() __annotate_reachable(__COUNTER__)
-
-#define __annotate_unreachable(c) ({					\
-	asm volatile(__stringify_label(c) ":\n\t"			\
+#define annotate_unreachable() ({					\
+	asm volatile("%c0:\n\t"						\
 		     ".pushsection .discard.unreachable\n\t"		\
-		     ".long " __stringify_label(c) "b - .\n\t"		\
-		     ".popsection\n\t");				\
+		     ".long %c0b - .\n\t"				\
+		     ".popsection\n\t" : : "i" (__COUNTER__ & 0x7f));	\
 })
-#define annotate_unreachable() __annotate_unreachable(__COUNTER__)
-
 #define ASM_UNREACHABLE							\
 	"999:\n\t"							\
 	".pushsection .discard.unreachable\n\t"				\
