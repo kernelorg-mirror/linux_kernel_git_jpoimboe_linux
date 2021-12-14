3668c533754cff51f0f79bb8d02d221481f37bb9
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2021-12-14 18:02:55 +0300

    x86/tdx: Get page shared bit info from the TDX Module
    Intel TDX doesn't allow VMM to access guest private memory. Any memory
    that is required for communication with the VMM must be shared
    explicitly by setting a bit in the page table entry. Details about
    which bit in the page table entry to be used to indicate shared/private
    state can be determined by using the TDINFO TDCALL (call to TDX
    Module).
    
    Fetch and save the guest TD execution environment information at
    initialization time. The next patch will use the information.
    
    More details about the TDINFO TDCALL can be found in
    Guest-Host-Communication Interface (GHCI) for Intel Trust Domain
    Extensions (Intel TDX) specification, sec titled "TDCALL[TDINFO]".
    
    Co-developed-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- arch/x86/kernel/tdx.c
+++ arch/x86/kernel/tdx.c
@@ -11,6 +11,7 @@
 #include <asm/insn-eval.h>
 
 /* TDX Module Call Leaf IDs */
+#define TDX_GET_INFO			1
 #define TDX_GET_VEINFO			3
 
 /* See Exit Qualification for I/O Instructions in VMX documentation */
@@ -18,6 +19,12 @@
 #define VE_GET_IO_SIZE(exit_qual)	(((exit_qual) & 7) + 1)
 #define VE_GET_PORT_NUM(exit_qual)	((exit_qual) >> 16)
 #define VE_IS_IO_STRING(exit_qual)	((exit_qual) & 16 ? 1 : 0)
+
+/* Guest TD execution environment information */
+static struct {
+	unsigned int gpa_width;
+	unsigned long attributes;
+} td_info __ro_after_init;
 
 static bool tdx_guest_detected __ro_after_init;
 
@@ -43,6 +50,28 @@ static inline u64 _tdx_hypercall(u64 fn, u64 r12, u64 r13, u64 r14,
 		panic("Hypercall fn %llu failed (Buggy TDX module!)\n", fn);
 
 	return out->r10;
+}
+
+static void tdx_get_info(void)
+{
+	struct tdx_module_output out;
+	u64 ret;
+
+	/*
+	 * TDINFO TDX Module call is used to get the TD execution environment
+	 * information like GPA width, number of available vcpus, debug mode
+	 * information, etc. More details about the ABI can be found in TDX
+	 * Guest-Host-Communication Interface (GHCI), sec 2.4.2 TDCALL
+	 * [TDG.VP.INFO].
+	 */
+	ret = __tdx_module_call(TDX_GET_INFO, 0, 0, 0, 0, &out);
+
+	/* Non zero return value indicates buggy TDX module, so panic */
+	if (ret)
+		panic("TDINFO TDCALL failed (Buggy TDX module!)\n");
+
+	td_info.gpa_width = out.rcx & GENMASK(5, 0);
+	td_info.attributes = out.rdx;
 }
 
 static __cpuidle u64 _tdx_halt(const bool irq_disabled, const bool do_sti)
@@ -447,6 +476,8 @@ void __init tdx_early_init(void)
 	tdx_guest_detected = true;
 
 	setup_force_cpu_cap(X86_FEATURE_TDX_GUEST);
+
+	tdx_get_info();
 
 	pr_info("Guest detected\n");
 }
