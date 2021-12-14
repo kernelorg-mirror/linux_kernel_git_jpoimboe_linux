dcd3b4b124f50d4225980a4fd73ee7ffb6ccd7a9
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2021-12-14 18:02:58 +0300

    x86/tdx: Add helper to convert memory between shared and private
    Intel TDX protects guest memory from VMM access. Any memory that is
    required for communication with the VMM must be explicitly shared.
    
    It is a two-step process: the guest sets the shared bit in the page
    table entry and notifies VMM about the change. The notification happens
    using MapGPA hypercall.
    
    Conversion back to private memory requires clearing the shared bit,
    notifying VMM with MapGPA hypercall following with accepting the memory
    with AcceptPage hypercall.
    
    Provide a helper to do conversion between shared and private memory.
    It is going to be used by the following patch.
    
    Co-developed-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -55,6 +55,15 @@ struct ve_info {
 	u32 instr_info;
 };
 
+/*
+ * Page mapping types. This is software construct not part of any hardware
+ * or VMM ABI.
+ */
+enum tdx_map_type {
+	TDX_MAP_PRIVATE,
+	TDX_MAP_SHARED,
+};
+
 #ifdef CONFIG_INTEL_TDX_GUEST
 
 void __init tdx_early_init(void);
@@ -78,6 +87,9 @@ bool tdx_early_handle_ve(struct pt_regs *regs);
 
 phys_addr_t tdx_shared_mask(void);
 
+int tdx_hcall_request_gpa_type(phys_addr_t start, phys_addr_t end,
+			       enum tdx_map_type map_type);
+
 #else
 
 static inline void tdx_early_init(void) { };
@@ -86,6 +98,12 @@ static inline void tdx_guest_idle(void) { };
 
 static inline bool tdx_early_handle_ve(struct pt_regs *regs) { return false; }
 static inline phys_addr_t tdx_shared_mask(void) { return 0; }
+
+static inline int tdx_hcall_request_gpa_type(phys_addr_t start, phys_addr_t end,
+					     enum tdx_map_type map_type)
+{
+	return -ENODEV;
+}
 
 #endif /* CONFIG_INTEL_TDX_GUEST */
 
