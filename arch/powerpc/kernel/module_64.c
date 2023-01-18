398e3acaee474b53184db9e8e059a7ffac01e842
Miroslav Benes <mbenes@suse.cz>
2023-01-18 12:47:28 -0800

    livepatch: Clear relocation targets on a module removal
    Josh reported a bug:
    
      When the object to be patched is a module, and that module is
      rmmod'ed and reloaded, it fails to load with:
    
      module: x86/modules: Skipping invalid relocation target, existing value is nonzero for type 2, loc 00000000ba0302e9, val ffffffffa03e293c
      livepatch: failed to initialize patch 'livepatch_nfsd' for module 'nfsd' (-8)
      livepatch: patch 'livepatch_nfsd' failed for module 'nfsd', refusing to load module 'nfsd'
    
      The livepatch module has a relocation which references a symbol
      in the _previous_ loading of nfsd. When apply_relocate_add()
      tries to replace the old relocation with a new one, it sees that
      the previous one is nonzero and it errors out.
    
      On ppc64le, we have a similar issue:
    
      module_64: livepatch_nfsd: Expected nop after call, got e8410018 at e_show+0x60/0x548 [livepatch_nfsd]
      livepatch: failed to initialize patch 'livepatch_nfsd' for module 'nfsd' (-8)
      livepatch: patch 'livepatch_nfsd' failed for module 'nfsd', refusing to load module 'nfsd'
    
    He also proposed three different solutions. We could remove the error
    check in apply_relocate_add() introduced by commit eda9cec4c9a1
    ("x86/module: Detect and skip invalid relocations"). However the check
    is useful for detecting corrupted modules.
    
    We could also deny the patched modules to be removed. If it proved to be
    a major drawback for users, we could still implement a different
    approach. The solution would also complicate the existing code a lot.
    
    We thus decided to reverse the relocation patching (clear all relocation
    targets on x86_64). The solution is not
    universal and is too much arch-specific, but it may prove to be simpler
    in the end.
    
    Signed-off-by: Miroslav Benes <mbenes@suse.cz>
    Signed-off-by: Song Liu <song@kernel.org>
    Acked-by: Miroslav Benes <mbenes@suse.cz>
    Co-developed-by: Song Liu <song@kernel.org>
    Reported-by: Josh Poimboeuf <jpoimboe@redhat.com>

--- arch/powerpc/kernel/module_64.c
+++ arch/powerpc/kernel/module_64.c
@@ -749,6 +749,16 @@ int apply_relocate_add(Elf64_Shdr *sechdrs,
 	return 0;
 }
 
+#ifdef CONFIG_LIVEPATCH
+void clear_relocate_add(Elf64_Shdr *sechdrs,
+		       const char *strtab,
+		       unsigned int symindex,
+		       unsigned int relsec,
+		       struct module *me)
+{
+}
+#endif
+
 #ifdef CONFIG_DYNAMIC_FTRACE
 int module_trampoline_target(struct module *mod, unsigned long addr,
 			     unsigned long *target)
