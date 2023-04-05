3b5370854edcf47aac834b98378eeff0435473d8
Josh Poimboeuf <jpoimboe@kernel.org>
2023-04-05 10:09:41 -0700

    x86/ibt: Move IBT selftest to asm
    With frame pointers and kernel IBT enabled, the following warning is
    seen:
    
      vmlinux.o: warning: objtool: ibt_selftest+0x11: sibling call from callable instruction with modified stack frame
    
    The problem is that objtool interprets the indirect branch in
    ibt_selftest() as a sibling call, and GCC inserts a (partial) frame
    pointer prologue right before it:
    
      0000 000000000003f550 <ibt_selftest>:
      0000    3f550:	f3 0f 1e fa          	endbr64
      0004    3f554:	e8 00 00 00 00       	call   3f559 <ibt_selftest+0x9>	3f555: R_X86_64_PLT32	__fentry__-0x4
      0009    3f559:	55                   	push   %rbp
      000a    3f55a:	48 8d 05 02 00 00 00 	lea    0x2(%rip),%rax        # 3f563 <ibt_selftest_ip>
      0011    3f561:	ff e0                	jmp    *%rax
    
    Note the inline asm is missing ASM_CALL_CONSTRAINT, so the 'push %rbp'
    happens before the indirect branch and the 'mov %rsp, %rbp' happens
    afterwards.
    
    Simplify the generated code and make it easier to understand for both
    tools and humans by moving the selftest to proper asm.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -609,7 +609,7 @@ static __always_inline void setup_cet(struct cpuinfo_x86 *c)
 	wrmsrl(MSR_IA32_S_CET, msr);
 	cr4_set_bits(X86_CR4_CET);
 
-	if (!ibt_selftest()) {
+	if (ibt_selftest()) {
 		pr_err("IBT selftest: Failed!\n");
 		wrmsrl(MSR_IA32_S_CET, 0);
 		setup_clear_cpu_cap(X86_FEATURE_IBT);
