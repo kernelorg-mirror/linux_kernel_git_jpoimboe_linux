b93c33fd3e929675fc1e82a6ceb0f2fce9b14213
Josh Poimboeuf <jpoimboe@kernel.org>
2024-08-31 19:27:31 -0700

    objtool: Use 'struct elf' in elf macros
    'struct objtool_file' is specific to the check code and doesn't belong
    in the elf code.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>

--- tools/objtool/include/objtool/elf.h
+++ tools/objtool/include/objtool/elf.h
@@ -296,16 +296,16 @@ static inline void set_reloc_type(struct elf *elf, struct reloc *reloc, unsigned
 	mark_sec_changed(elf, reloc->sec, true);
 }
 
-#define for_each_sec(file, sec)						\
-	list_for_each_entry(sec, &file->elf->sections, list)
+#define for_each_sec(elf, sec)						\
+	list_for_each_entry(sec, &elf->sections, list)
 
 #define sec_for_each_sym(sec, sym)					\
 	list_for_each_entry(sym, &sec->symbol_list, list)
 
-#define for_each_sym(file, sym)						\
+#define for_each_sym(elf, sym)						\
 	for (struct section *__sec, *__fake = (struct section *)1;	\
 	     __fake; __fake = NULL)					\
-		for_each_sec(file, __sec)				\
+		for_each_sec(elf, __sec)				\
 			sec_for_each_sym(__sec, sym)
 
 #define for_each_reloc(rsec, reloc)					\
