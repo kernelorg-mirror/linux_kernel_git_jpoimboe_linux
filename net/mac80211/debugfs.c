2bf7c4a3699a4b296c327f553deb62d1f15d2843
Josh Poimboeuf <jpoimboe@kernel.org>
2024-10-02 18:46:42 -0700

    wifi: mac80211: Simplify aql_disable static key enabling/disabling
    Instead of checking if manually checking whether the key is enabled
    before modifying it, just use static_branch_{enable,disable}() which
    already do that.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>

--- net/mac80211/debugfs.c
+++ net/mac80211/debugfs.c
@@ -327,7 +327,6 @@ static ssize_t aql_enable_read(struct file *file, char __user *user_buf,
 static ssize_t aql_enable_write(struct file *file, const char __user *user_buf,
 				size_t count, loff_t *ppos)
 {
-	bool aql_disabled = static_key_enabled(&aql_disable.key);
 	char buf[3];
 	size_t len;
 
@@ -343,11 +342,9 @@ static ssize_t aql_enable_write(struct file *file, const char __user *user_buf,
 		buf[len - 1] = 0;
 
 	if (buf[0] == '0' && buf[1] == '\0') {
-		if (!aql_disabled)
-			static_branch_inc(&aql_disable);
+		static_branch_enable(&aql_disable);
 	} else if (buf[0] == '1' && buf[1] == '\0') {
-		if (aql_disabled)
-			static_branch_dec(&aql_disable);
+		static_branch_disable(&aql_disable);
 	} else {
 		return -EINVAL;
 	}
