eb96383e79a1a9cf758ef51ded5ab9db17cde304
Josh Poimboeuf <jpoimboe@kernel.org>
2025-03-07 15:10:20 -0800

    x86/asm: Disable asm_call() output constraint for non-FP configs
    On non-frame-pointer configs, the stack pointer output constraint
    affects code generation unnecessarily.
    
    The constraint is only needed for frame pointers anyway.  Disable it for
    non-FP configs.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>

--- arch/x86/include/asm/asm.h
+++ arch/x86/include/asm/asm.h
@@ -206,7 +206,12 @@ static __always_inline __pure void *rip_rel_ptr(void *p)
 
 register unsigned long current_stack_pointer asm(_ASM_SP);
 
-#define ASM_CALL_OUTPUT(x...)	ASM_CALL_CONSTRAINT, ## x
+#ifdef CONFIG_FRAME_POINTER_UNWINDER
+# define ASM_CALL_OUTPUT(...)	"+r" (current_stack_pointer), ##__VA_ARGS__
+#else
+# define ASM_CALL_OUTPUT(x...)	x
+#endif
+
 #define ASM_CALL_INPUT(x...)	x
 #define ASM_CALL_CLOBBER(x...)	"memory", ## x
 
