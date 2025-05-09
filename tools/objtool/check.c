6c22915a4ef67a05c64dd0855021d1d7426409f8
Josh Poimboeuf <jpoimboe@kernel.org>
2025-05-09 12:58:04 -0700

    objtool: Clean up compiler flag usage
    KBUILD_HOSTCFLAGS and KBUILD_HOSTLDFLAGS aren't defined when objtool is
    built standalone.  Also, the EXTRA_WARNINGS flags are rather arbitrary.
    
    Make things simpler and more consistent by specifying compiler flags
    explicitly and tweaking the warnings.  Also make a few code tweaks to
    make the new warnings happy.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -460,7 +460,7 @@ static int decode_instructions(struct objtool_file *file)
 
 		for (offset = 0; offset < sec->sh.sh_size; offset += insn->len) {
 			if (!insns || idx == INSN_CHUNK_MAX) {
-				insns = calloc(sizeof(*insn), INSN_CHUNK_SIZE);
+				insns = calloc(INSN_CHUNK_SIZE, sizeof(*insn));
 				if (!insns) {
 					ERROR_GLIBC("calloc");
 					return -1;
@@ -606,7 +606,7 @@ static int init_pv_ops(struct objtool_file *file)
 		return 0;
 
 	nr = sym->len / sizeof(unsigned long);
-	file->pv_ops = calloc(sizeof(struct pv_state), nr);
+	file->pv_ops = calloc(nr, sizeof(struct pv_state));
 	if (!file->pv_ops) {
 		ERROR_GLIBC("calloc");
 		return -1;
