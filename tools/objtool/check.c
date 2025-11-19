e8064e2bd2ff7cd56b240efea75a1bdb796a3b7e
Alexandre Chartre <alexandre.chartre@oracle.com>
2025-11-19 15:32:20 +0100

    objtool: Disassemble code with libopcodes instead of running objdump
    objtool executes the objdump command to disassemble code. Use libopcodes
    instead to have more control about the disassembly scope and output.
    If libopcodes is not present then objtool is built without disassembly
    support.
    
    Signed-off-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Link: https://patch.msgid.link/20251119143247.3944213-4-alexandre.chartre@oracle.com
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -4959,8 +4959,6 @@ int check(struct objtool_file *file)
 			goto out;
 	}
 
-	free_insns(file);
-
 	if (opts.stats) {
 		printf("nr_insns_visited: %ld\n", nr_insns_visited);
 		printf("nr_cfi: %ld\n", nr_cfi);
@@ -4969,8 +4967,10 @@ int check(struct objtool_file *file)
 	}
 
 out:
-	if (!ret && !warnings)
+	if (!ret && !warnings) {
+		free_insns(file);
 		return 0;
+	}
 
 	if (opts.werror && warnings)
 		ret = 1;
@@ -4979,9 +4979,13 @@ out:
 		if (opts.werror && warnings)
 			WARN("%d warning(s) upgraded to errors", warnings);
 		disas_ctx = disas_context_create(file);
-		disas_warned_funcs(disas_ctx);
-		disas_context_destroy(disas_ctx);
+		if (disas_ctx) {
+			disas_warned_funcs(disas_ctx);
+			disas_context_destroy(disas_ctx);
+		}
 	}
+
+	free_insns(file);
 
 	if (opts.backup && make_backup())
 		return 1;
