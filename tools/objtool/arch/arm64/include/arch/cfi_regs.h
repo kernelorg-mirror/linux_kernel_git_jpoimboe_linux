7478abf5b2e4e5803e3721f2218822897bdc2970
Josh Poimboeuf <jpoimboe@kernel.org>
2026-03-02 19:43:24 -0800

    objtool/klp: Introduce objtool for arm64
    Add basic support for arm64 on objtool.  Only "objtool klp" subcommands
    are currently supported.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>

---
+++ tools/objtool/arch/arm64/include/arch/cfi_regs.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _OBJTOOL_ARCH_CFI_REGS_H
+#define _OBJTOOL_ARCH_CFI_REGS_H
+
+/* These aren't actually used for arm64 */
+#define CFI_BP 0
+#define CFI_SP 0
+#define CFI_RA 0
+#define CFI_NUM_REGS 2
+
+#endif /* _OBJTOOL_ARCH_CFI_REGS_H */
